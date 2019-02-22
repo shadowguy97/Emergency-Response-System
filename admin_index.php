fe3161d9560efbf660663486cf3376aff1705be9
Shadow <olajidepelumi@gmail.com>
2019-02-22 10:02:35 +0100

    msg: renamed index.php to admin_index.php and report.php to index.php

---
+++ admin_index.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+  <title>EMS: Emergency Medical System </title>
+
+  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+  <meta name="viewport" content="width=device-width" />
+
+  <!-- Bootstrap core CSS     -->
+  <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+  <link href="assets/css/style.css" rel="stylesheet" />
+  <link rel="stylesheet" href="assets/css/Pretty-Header.css">
+  <link rel="stylesheet" href="assets/css/Hero-Technology.css">
+    
+
+  <!--     Fonts and icons     -->
+  <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+  <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
+  <link href="assets/css/pe-icon-7-stroke.css" rel="stylesheet" />
+  <link href="assets/css/font-awesome.min.css" rel="stylesheet" />
+</head>
+
+<body>
+    <div class="col-md-12">
+        <nav class="navbar navbar-default custom-header">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+                </div>
+                <div class="collapse navbar-collapse" id="navbar-collapse">
+                    <ul class="nav navbar-nav navbar-right links">
+                        <li role="presentation"><a href="index.php">Home </a></li>
+                        <li role="presentation"><a href="login.php">Login </a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="jumbotron hero-technology">
+                <h1 class="hero-title">Emergency Response System.</h1>
+                <p class="hero-subtitle">This site is made so as to help manage reported emergency situations and various Emergency Service Provider. <br>
+                Please click the button below to Login</p>
+                <p><a class="btn btn-primary btn-lg hero-button" role="button" href="login.php">Login</a></p>
+            </div>
+        </div>
+    </div>
+
+
+</body>
+
+<!--   Core JS Files   -->
+<script src="assets/js/jquery-1.12.4.js" type="text/javascript"></script>
+<script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+
+</html>
